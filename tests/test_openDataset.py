c3a1ab31d2232b9da76d6502d882e73e7dbc3493
miniufo <miniufo@163.com>
2024-10-26 11:25:28 +0800

    fixed auto tests

--- tests/test_openDataset.py
+++ tests/test_openDataset.py
@@ -19,10 +19,12 @@ def test_template():
     use_close = True if sys.version_info[0] == 3 and sys.version_info[1]>8 else False
     
     if use_close:
+        print('1')
         for l in range(len(dset1.time)):
             xr.testing.assert_allclose(dset1.air[l], dset2.air[l])
             xr.testing.assert_allclose(dset1.air[l], dset3.air[l])        
     else:
+        print('2')
         for l in range(len(dset1.time)):
             xr.testing.assert_equal(dset1.air[l], dset2.air[l])
             xr.testing.assert_equal(dset1.air[l], dset3.air[l])
@@ -33,26 +35,30 @@ def test_template():
     dset3 = xr.tutorial.open_dataset('air_temperature').load().astype('>f4')
     
     if use_close:
+        print('3')
         for l in range(len(dset1.time)):
             xr.testing.assert_allclose(dset1.air[l], dset3.air[l])        
     else:
+        print('4')
         for l in range(len(dset1.time)):
             xr.testing.assert_equal(dset1.air[l], dset3.air[l])
     
-    
+    print('5')
     dset1 = open_mfdataset('./ctls/test9_*.ctl', parallel=True)
     dset2 = open_CtlDataset('./ctls/test9.ctl').load()
     
     for l in range(len(dset1.time)):
         xr.testing.assert_equal(dset1.air[l], dset2.air[l])
     
-    
+    print('6')
     # test blank line in ctls
     dset1 = open_CtlDataset('./ctls/test81.ctl')
     dset2 = open_CtlDataset('./ctls/test82.ctl')
     
     assert (dset1.x == dset2.x).all()
+    print('7')
     assert (dset1.y == dset2.y).all()
+    print('8')
     assert (dset1.air[0] == dset2.air).all()
 
 def test_ensemble():
