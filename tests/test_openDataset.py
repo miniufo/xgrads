e900f93a87b5f8fb2c85a2d435082326776bd8d7
miniufo <miniufo@163.com>
2024-10-28 18:13:32 +0800

    fixed auto tests

--- tests/test_openDataset.py
+++ tests/test_openDataset.py
@@ -32,14 +32,16 @@ def test_template():
     
     dset1 = open_mfdataset('./ctls/test8_*.ctl', parallel=True)
     dset2 = open_CtlDataset('./ctls/test8.ctl').load()
-    dset3 = xr.tutorial.open_dataset('air_temperature').load().astype('>f4')
+    dset33 = xr.tutorial.open_dataset('air_temperature').load().astype('>f4')
     
     if use_close:
         print('3')
         print(dset1)
-        print(dset3)
+        print(dset2)
+        print(dset33)
         for l in range(len(dset1.time)):
-            print(dset1.time[l].values, dset3.time[l].values)
+            print('dset1 and 3 ', dset1.time[l].values, dset3.time[l].values)
+            print('dset1 and 33 ', dset1.time[l].values, dset33.time[l].values)
             print(dset1.air[:,0,0].values, dset3.air[:10,0,0].values)
             xr.testing.assert_allclose(dset1.air[l], dset3.air[l])        
     else:
