3d07b5cb4b0a661d52f64719ac6d3591be76d990
miniufo <miniufo@163.com>
2024-10-28 20:46:55 +0800

    fixed auto tests

--- tests/test_openDataset.py
+++ tests/test_openDataset.py
@@ -12,8 +12,8 @@ from xgrads import open_CtlDataset, open_mfdataset
 
 
 def test_template():
-    dset1 = open_CtlDataset('./ctls/test8.ctl')
-    dset2 = open_CtlDataset('./ctls/test9.ctl')
+    dset1 = open_CtlDataset('./ctls/test8.ctl').load()
+    dset2 = open_CtlDataset('./ctls/test9.ctl').load()
     dset3 = xr.tutorial.open_dataset('air_temperature').load().astype('>f4')
     
     use_close = True if sys.version_info[0] == 3 and sys.version_info[1]>8 else False
@@ -30,19 +30,23 @@ def test_template():
             xr.testing.assert_equal(dset1.air[l], dset3.air[l])
 
     
-    dset1 = open_mfdataset('./ctls/test8_*.ctl', parallel=True)
-    dset2 = open_CtlDataset('./ctls/test8.ctl').load()
+    dset11 = open_mfdataset('./ctls/test8_*.ctl', parallel=True).load()
+    dset22 = open_CtlDataset('./ctls/test8.ctl').load()
     dset33 = xr.tutorial.open_dataset('air_temperature').load().astype('>f4')
     
     if use_close:
         print('3')
         print(dset1)
         print(dset2)
+        print(dset3)
+        print(dset11)
+        print(dset22)
         print(dset33)
         for l in range(len(dset1.time)):
             print('dset1 and 3 ', dset1.time[l].values, dset3.time[l].values)
             print('dset1 and 33 ', dset1.time[l].values, dset33.time[l].values)
-            print(dset1.air[:,0,0].values, dset3.air[:10,0,0].values, dset33.air[:10,0,0].values)
+            print(dset1.air[:,0,0].values, dset2.air[:5,0,0].values, dset3.air[:5,0,0].values)
+            print(dset11.air[:,0,0].values, dset22.air[:5,0,0].values, dset33.air[:5,0,0].values)
             xr.testing.assert_allclose(dset1.air[l], dset3.air[l])        
     else:
         print('4')
